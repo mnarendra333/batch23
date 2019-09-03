9680edfbad156726755a1931fb6c0178e2524f3d
Narendra <mnarendra333@gmail.com>
2019-09-03 09:08:25 +0530

    added few prog on OOPS

---
+++ OOPSConcepts/Student.java
@@ -0,0 +1,23 @@
+class Student
+{
+
+		int id;
+		String name;
+		int marks;
+		
+		
+		
+		
+		public static void main(String... args)
+		{
+			
+			Student obj = new Student();
+
+			System.out.println(obj.id+" "+obj.name+" "+obj.marks);
+			
+		}
+		
+
+}
+
+//op - should print default values
---
+++ OOPSConcepts/Student2.java
@@ -0,0 +1,25 @@
+class Student2
+{
+
+		int id=101;
+		String name="Raja";
+		int marks=500;
+		
+		
+		
+		
+		public static void main(String... args)
+		{
+			
+			Student2 obj = new Student2();
+
+			System.out.println(obj.id+" "+obj.name+" "+obj.marks);
+			
+			Student2 obj2 = new Student2();
+
+			System.out.println(obj2.id+" "+obj2.name+" "+obj2.marks);
+			
+		}
+		
+
+}
