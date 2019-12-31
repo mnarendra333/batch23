fa13d945b1f19cf88e7f3e68f0b6e4ac0eea0834
Narendra <mnarendra333@gmail.com>
2019-12-31 09:23:58 +0530

    added a springmvc app

---
+++ spring_ws/springmvcfirstapp/src/main/java/com/pragim/springmvcfirstapp/controller/EmployeeController.java
@@ -0,0 +1,19 @@
+package com.pragim.springmvcfirstapp.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+@RequestMapping("/employee")
+public class EmployeeController {
+	
+	
+	@RequestMapping("/welcome")
+	public ModelAndView welcome(){
+	
+		ModelAndView obj = new ModelAndView("success", "msg", "Welcome to Spring MVC Framework!!!");
+		return obj;
+	}
+
+}
